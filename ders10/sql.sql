12ae45b4b5a2094c9f089abfac6ae04736e44827
Volkan KOÇ <volkankoc53@gmail.com>
2021-01-13 11:12:48 +0300

    güncel sql 13 ocak

--- ders10/sql.sql
+++ ders10/sql.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Jan 07, 2021 at 09:19 AM
+-- Generation Time: Jan 13, 2021 at 08:12 AM
 -- Server version: 5.7.26
 -- PHP Version: 7.4.2
 
@@ -17,81 +17,35 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `sessions`
+-- Table structure for table `posts`
 --
 
-CREATE TABLE `sessions` (
+CREATE TABLE `posts` (
   `id` int(11) NOT NULL,
-  `session_id` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
-  `user` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
-  `giris_saati` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `cikis_saati` varchar(50) COLLATE utf8_turkish_ci NOT NULL
+  `title` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `body` varchar(1000) COLLATE utf8_turkish_ci NOT NULL,
+  `author` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
 
 --
--- Dumping data for table `sessions`
+-- Dumping data for table `posts`
 --
 
-INSERT INTO `sessions` (`id`, `session_id`, `user`, `giris_saati`, `cikis_saati`) VALUES
-(1, '0', 'volkan', '2021-01-07 08:02:22', '2021-01-07 08:51:23'),
-(2, '0', 'ahmet', '2021-01-07 08:03:11', '2021-01-07 08:47:09'),
-(3, '0', 'volkan', '2021-01-07 08:03:22', '2021-01-07 11:03:22'),
-(4, '0', 'volkan', '2021-01-07 08:27:20', '2021-01-07 11:27:20'),
-(5, '0', 'volkan', '2021-01-07 08:34:18', '2021-01-07 11:34:18'),
-(6, '0', 'ahmet', '2021-01-07 08:40:05', '2021-01-07 11:40:05'),
-(7, '0', 'volkan', '2021-01-07 08:44:28', '2021-01-07 11:44:28'),
-(8, '0', 'ahmet', '2021-01-07 08:46:52', '2021-01-07 11:46:52'),
-(9, '0', 'volkan', '2021-01-07 08:51:08', ''),
-(10, '2', 'ahmet', '2021-01-07 08:55:05', '2021-01-07 08:56:27'),
-(11, '1', 'volkan', '2021-01-07 08:56:56', '2021-01-07 08:57:43'),
-(12, '1', 'volkan', '2021-01-07 08:57:23', '2021-01-07 08:57:43'),
-(13, '2', 'ahmet', '2021-01-07 09:00:25', ''),
-(14, 'jma8i5t7f16cp08a5sp8h5e8hi', 'volkan', '2021-01-07 09:08:06', '2021-01-07 09:14:31'),
-(15, 'jma8i5t7f16cp08a5sp8h5e8hi', 'volkan', '2021-01-07 09:09:57', '2021-01-07 09:14:31'),
-(16, 'jma8i5t7f16cp08a5sp8h5e8hi', 'ahmet', '2021-01-07 09:10:26', '2021-01-07 09:14:31'),
-(17, 'jma8i5t7f16cp08a5sp8h5e8hi', 'ahmet', '2021-01-07 09:11:39', '2021-01-07 09:14:31'),
-(18, 'jac6guugnr49v5loin5fcehlpp', 'ahmet', '2021-01-07 09:12:25', '2021-01-07 09:13:02'),
-(19, 'jma8i5t7f16cp08a5sp8h5e8hi', 'volkan', '2021-01-07 09:13:21', '2021-01-07 09:14:31'),
-(20, 'm3vngmjagd3muslufohq826vo7', 'ahmet', '2021-01-07 09:14:41', '2021-01-07 09:14:58'),
-(21, 'e55ahbg3f93rr5hvf4c7777efg', 'volkan', '2021-01-07 09:15:18', '2021-01-07 09:15:34'),
-(22, 'hlqeacs525377ebv8krdtg75oj', 'volkan', '2021-01-07 09:18:19', '2021-01-07 12:18:29');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
-  `username` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
-  `pass` varchar(50) COLLATE utf8_turkish_ci NOT NULL,
-  `role` varchar(10) COLLATE utf8_turkish_ci NOT NULL,
-  `adsoyad` varchar(50) COLLATE utf8_turkish_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `pass`, `role`, `adsoyad`) VALUES
-(1, 'volkan', '827ccb0eea8a706c4c34a16891f84e7b', 'admin', 'volkan koç'),
-(2, 'ahmet', '6074c6aa3488f3c2dddff2a7ca821aab', 'user', 'ahmet güntepe');
+INSERT INTO `posts` (`id`, `title`, `body`, `author`, `date`) VALUES
+(1, 'WhatsApp Veri İhlali', 'WhatsApp, birkaç yıl önce Facebook tarafından satın alınmıştı. Uzun yıllardır insanlar, Zuckerberg\'in nasıl olup da uygulamayı hala reklama boğmadan, para istemeden kullandırmaya devam ettiğini de merak ediyordu. \r\n\r\nWhatsApp\'ın yeni gizlilik ilkeleri, pek çok kişinin tepkisini çekmiş durumda. Daha önceki haberlerimizde de belirttiğimiz üzere yeni gizlilik ilkeleri kapsamında Facebook şirketi (site değil, sitenin de sahibi olan şirket), neredeyse DNA dizilimimiz dışında her şeye erişim istiyor. Erişim vermeyenler de uygulamayı kullanamıyor.', 'volkan koç', '2021-01-13 07:23:45'),
+(2, 'Yerli Whatsapp', 'Aslına bakarsak bugüne kadar WhatsApp\'ın kullanıcı verilerini Facebook\'a verip vermediği hep bir tartışma konusu olmuştu. Hatta bu iddialar, Zuckerberg\'in senatoda yaptığı savunmada bile ima edilmişti. Şu andaki durum ise daha çok malumun ilamı oluyor. \r\n\r\nKullanıcıların yeni sözleşmeyi kabul etmeleri halinde Facebook ile paylaşmayı taahhüt ettiği bilgilere de bir bakalım. Bu bilgilerin oldukça uzun ve okudukça sinir bozan bir listesi var. Liste şu şekilde:\r\n\r\nHesap Bilgileri\r\nMesajlar\r\nBağlantılar\r\nDurum Bilgisi\r\nİşlem ve Ödeme Verileri\r\nMüşteri Desteği ve Diğer İletişimler\r\nKullanım ve Kayıt Bilgileri', 'ali', '2021-01-13 07:34:47'),
+(3, 'Yerli Video konferans', 'Facebook\'un açıklamasına göre bu verileri sayesinde firmanın bünyesindeki farklı sosyal medya kanallarından gelen veriler entegre edilecek. Böylece yan hizmetler geliştirilecek. Sistemin emniyeti, güvenliği ve bütünlüğü artırılacak. Facebook şirketleri arasındaki iletişim ve kurumsal etkileşimler artacak.\r\n\r\nHali hazırda pek çok kişinin \"Arkadaşımla konuşurken elma dedim, eve bir geldim aaa elma reklamı\" demesine neden olan sistemler artık resmiyet kazanmış olacak. Adım adım Kalite Ülkesi adlı kitaba doğru ilerlediğimiz şu günlerde artık algoritmalar sayesinde kullanıcıların düşünmesi muhtemel ürünlerin bile reklamlarını gördüğümüzü göz önüne alırsak, Zukerberg\'in tek yaptığı işleri resmiyete dökerken kullanıcılara fikir sormaktan ibaret. ', 'volkan', '2021-01-13 07:34:47'),
+(4, 'Yeni İleti', 'Kullanım ve Kayıt Bilgileri\r\nCihaz ve Bağlantı Bilgileri\r\nKonum Bilgileri\r\nÇerezler\r\nBaşkalarının Sizin Hakkınızda Sağladığı Bilgiler (Üçüncü taraftan sağlanır.)\r\nKullanıcı Şikayetleri (Üçüncü taraftan sağlanır.)\r\nWhatsApp’taki İşletmeler (Üçüncü taraftan sağlanır.)\r\nÜçüncü Taraf Hizmet Sağlayıcıları (Üçüncü taraftan sağlanır.)\r\nÜçüncü Taraf Hizmetleri (Üçüncü taraftan sağlanır.)', 'volkan', '2021-01-13 07:57:33');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `sessions`
+-- Indexes for table `posts`
 --
-ALTER TABLE `sessions`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
+ALTER TABLE `posts`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -99,13 +53,7 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `sessions`
+-- AUTO_INCREMENT for table `posts`
 --
-ALTER TABLE `sessions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+ALTER TABLE `posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
